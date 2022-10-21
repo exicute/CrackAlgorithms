c69e2f3df17d42b8ae3e393f4cb50be4a215c28f
exicute <secret.exicute@gmail.com>
2022-10-21 22:44:34 +0500

    определено количество пикселей каждого цвета(hue) и их координаты, далее можно использую статистику находить нужные цвета

--- imgprocess.py
+++ imgprocess.py
@@ -28,8 +28,22 @@ def change_hsv(path):
     image = cv.imread(path)
     hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     
-    np.multiply(2, hsv[:,:,1], out=hsv[:,:,1])
-    hsv[:,:,1] = np.where((hsv[:,:,1]<80), hsv[:,:,1]*3, 0)
+    #np.multiply(2, hsv[:,:,1], out=hsv[:,:,1])
+    #hsv[:,:,1] = np.where((hsv[:,:,1]<80), hsv[:,:,1]*3, 0)
+
+    HueUnique = np.unique(hsv[:,:,0])
+    HueUnique = HueUnique.astype('int16')
+    HueUnique = HueUnique.reshape(179, 1)
+    hue_values = np.concatenate((HueUnique, np.ones((179, 1), dtype='int32')), axis=1)
+
+    for u in range(HueUnique.size):
+            hue_values[u, 1] = np.sum(hsv[:,:,0]==hue_values[u, 0])
+
+    hues = hue_values[:,1]>80
+    hues = hue_values[hues]
+
+    hsv[:,:,1] = np.where((hsv[:,:,0] in hues[:,0]), hsv[:,:,1], hsv[:,:,1]//2)
+    hsv[:,:,2] = np.where((hsv[:,:,0] in hues[:,0]), hsv[:,:,2],hsv[:,:,2]//2)
 
     hsv = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
 
